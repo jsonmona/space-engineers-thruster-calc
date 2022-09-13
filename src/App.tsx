11bb3258afb15c6b95305b231110c95bb03c3242
jsonmona <jsonmona@outlook.com>
2022-09-13 17:34:07 +0900

    feat: Add a background image

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from '@emotion/styled';
 import FieldsView from "./components/FieldsView";
 import FieldsContext from "./components/FieldsContext";
+import bg from './bg.jpg';
 
 const AppBox = styled('div')`
-  margin: 1rem;
+  padding: 1rem;
+  background: no-repeat bottom right/50% url(${bg});
+  height: 100vh;
 `;
 
 function App() {
